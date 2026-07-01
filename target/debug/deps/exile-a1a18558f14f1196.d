8d21f127708e15986fdd1fe7e0c927b90ef0a502
bedolaga <zpavlenko054@gmail.com>
2026-07-01 23:37:01 +0300

    Initial commit workspace

---
+++ target/debug/deps/exile-a1a18558f14f1196.d
@@ -0,0 +1,9 @@
+D:\rustExile\target\debug\deps\exile-a1a18558f14f1196.d: exile\src\main.rs Cargo.toml
+
+D:\rustExile\target\debug\deps\libexile-a1a18558f14f1196.rmeta: exile\src\main.rs Cargo.toml
+
+exile\src\main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ target/debug/deps/exile_core-1c686ca79d653738.d
@@ -0,0 +1,9 @@
+D:\rustExile\target\debug\deps\exile_core-1c686ca79d653738.d: exile-core\src\lib.rs Cargo.toml
+
+D:\rustExile\target\debug\deps\libexile_core-1c686ca79d653738.rmeta: exile-core\src\lib.rs Cargo.toml
+
+exile-core\src\lib.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
