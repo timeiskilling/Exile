742ec3eb996ef58762c7db06b3698192a55d3cac
bedolaga <zpavlenko054@gmail.com>
2026-08-03 17:16:02 +0300

    Update game.rs

--- exile-poe2/src/game.rs
+++ exile-poe2/src/game.rs
@@ -2,6 +2,48 @@ use exile_core::game::*;
 
 pub struct Poe2;
 
+pub enum DamageType {
+    Physical,
+    Fire,
+    Cold,
+    Lightning,
+    Chaos,
+}
+
+pub enum ItemBase {
+    Amulet,
+    Armor,
+    Belt,
+    Boots,
+    Bow,
+    Buckler,
+    Claw,
+    Crossbow,
+    Dagger,
+    FishingRod,
+    Flail,
+    Focus,
+    Gloves,
+    Helmet,
+    OneHandedAxe,
+    OneHandedMace,
+    OneHandedSword,
+    Quarterstaff,
+    Quiver,
+    Ring,
+    Scepter,
+    Shield,
+    SkillGem,
+    Spear,
+    Staff,
+    SupportGem,
+    Talisman,
+    TwoHandedAxe,
+    TwoHandedMace,
+    TwoHandedSword,
+    Wand,
+}
+
 #[derive(Debug, Default, PartialEq)]
 pub struct ItemState {
     item_level: u16,
@@ -11,16 +53,12 @@ pub struct ItemState {
 }
 
 impl Game for Poe2 {
-    type ItemBase;
+    type ItemBase = ItemBase;
     type ItemState = ItemState;
     type ModifierDefinitionId;
     type ModifierDefinition;
-
     type ModifierInstance;
-
     type Effect;
-
     type EffectCondition;
-
     type EffectSourceId;
 }
