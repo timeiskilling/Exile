2f6371290be9fbef872c786a56abeafe77fd1507
bedolaga <zpavlenko054@gmail.com>
2026-08-06 22:52:59 +0300

    Update repoe_parse.rs

--- exile-poe2/src/repoe_parse.rs
+++ exile-poe2/src/repoe_parse.rs
@@ -1,4 +1,8 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Deserializer};
+
+type Poe2Item = HashMap<String, Item>;
 
 struct Class {
     category: String,
@@ -81,4 +85,93 @@ where
 {
     let opt: Option<u32> = Option::deserialize(deserializer)?;
     Ok(opt.map(|val| if val == 0 { 0.0 } else { 1000.0 / (val as f32) }))
+}
+
+#[derive(Debug, Deserialize)]
+pub struct RawMod {
+    pub adds_tags: Vec<String>,
+    pub domain: Domain,
+    pub generation_type: GenerationType,
+    pub generation_weights: Vec<TagWeight>,
+    pub grants_effects: Vec<GrantedEffectRef>,
+    pub groups: Vec<String>,
+    pub implicit_tags: Vec<String>,
+    pub is_essence_only: bool,
+    pub name: String,
+    pub required_level: u32,
+    pub spawn_weights: Vec<TagWeight>,
+    pub stats: Vec<StatRoll>,
+    pub text: Option<String>,
+    #[serde(rename = "type")]
+    pub mod_type: String,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct TagWeight {
+    pub tag: String,
+    pub weight: u32,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct StatRoll {
+    pub id: String,
+    pub min: i64,
+    pub max: i64,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct GrantedEffectRef {
+    pub granted_effect_id: String,
+    pub level: u32,
+}
+
+pub type RawModsFile = HashMap<String, RawMod>;
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(rename_all = "lowercase")]
+pub enum GenerationType {
+    Prefix,
+    Suffix,
+    Corrupted,
+    Unique,
+    Essence,
+    #[serde(other)]
+    Unknown,
+}
+
+#[derive(Debug, Deserialize, PartialEq, Clone, Copy)]
+#[serde(rename_all = "snake_case")]
+pub enum Domain {
+    Item,
+    Flask,
+    Misc,
+    Crafted,
+    Monster,
+    Area,
+    HeistNpc,
+    Desecrated,
+    Chest,
+    HeistArea,
+    Strongbox,
+    SanctumRelic,
+    DelveArea,
+    Atlas,
+    Tablet,
+    HeistTrinket,
+    SynthesisA,
+    MapDevice,
+    SynthesisGlobals,
+    ExpeditionRelic,
+    Sentinel,
+    SynthesisBonus,
+    MemoryLine,
+    UltimatumKey,
+    Veiled,
+    VaultKey,
+    IncursionLimb,
+    Leaguestone,
+    Dummy,
+    AfflictionJewel,
+    #[serde(other)]
+    Unknown,
 }
